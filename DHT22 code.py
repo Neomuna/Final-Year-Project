e1f89d9248d7bd96610e5837da39059eaa3c0c74
Neomuna <125609735+Neomuna@users.noreply.github.com>
2026-02-25 15:30:19 +0000

    Add files via upload

--- DHT22 code.py
+++ DHT22 code.py
@@ -1,19 +1,11 @@
-# DHT22 code
+import pigpio
+import DHT22
 import time
 
-import adafruit_dht
-import board
-
-dht = adafruit_dht.DHT22(board.D2)
+pi = pigpio.pi()
+sensor = DHT22.sensor(pi, 4)  # GPIO4
 
 while True:
-    try:
-        temperature = dht.temperature
-        humidity = dht.humidity
-        # Print what we got to the REPL
-        print("Temp: {:.1f} *C \t Humidity: {}%".format(temperature, humidity))
-    except RuntimeError as e:
-        # Reading doesn't always work! Just print error and we'll try again
-        print("Reading from DHT failure: ", e.args)
-
+    sensor.trigger()
+    print(f"Temperature: {sensor.temperature()}°C  Humidity: {sensor.humidity()}%")
     time.sleep(1)
