3a4479304dec63c05024b20f2737e0eb75cc2fb4
Neomuna <125609735+Neomuna@users.noreply.github.com>
2026-02-25 15:37:18 +0000

    Updated DHT22 code
    Code which uses gpiozero like MQ7 - i will update SGP30 in due course

--- DHT22 code.py
+++ DHT22 code.py
@@ -1,11 +1,26 @@
-import pigpio
-import DHT22
-import time
+# DHT22 code for Raspberry Pi
 
-pi = pigpio.pi()
-sensor = DHT22.sensor(pi, 4)  # GPIO4
+# pip these packages before running the code:
+#pip install adafruit-circuitpython-dht
+#sudo apt install libgpiod2
+
+
+import time
+import board
+import adafruit_dht
+
+# GPIO4 = BCM 4
+dhtDevice = adafruit_dht.DHT22(board.D4)
 
 while True:
-    sensor.trigger()
-    print(f"Temperature: {sensor.temperature()}°C  Humidity: {sensor.humidity()}%")
+    try:
+        temperature = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+
+        print(f"Temperature: {temperature}°C  Humidity: {humidity}%")
+
+    except RuntimeError as error:
+        # DHT sensors are an issue, this should resolve itself
+        print(f"Reading error: {error}")
+
     time.sleep(1)
