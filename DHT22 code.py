9a12fbffee99beca6d3de892ff5dd2ac0777e7d9
Neomuna <125609735+Neomuna@users.noreply.github.com>
2026-03-01 16:10:48 +0000

    Refactored DHT22 code

--- DHT22 code.py
+++ DHT22 code.py
@@ -2,25 +2,20 @@
 
 # pip these packages before running the code:
 #pip install adafruit-circuitpython-dht
-#sudo apt install libgpiod2
+
 
 
 import time
 import board
 import adafruit_dht
 
-# GPIO4 = BCM 4
-dhtDevice = adafruit_dht.DHT22(board.D4)
+dht = adafruit_dht.DHT22(board.D4)
 
 while True:
     try:
-        temperature = dhtDevice.temperature
-        humidity = dhtDevice.humidity
-
-        print(f"Temperature: {temperature}°C  Humidity: {humidity}%")
-
-    except RuntimeError as error:
-        # DHT sensors are an issue, this should resolve itself
-        print(f"Reading error: {error}")
-
-    time.sleep(1)
+        print("Temp:", dht.temperature)
+        print("Humidity:", dht.humidity)
+    except RuntimeError:
+        pass
+    time.sleep(2)
+    
